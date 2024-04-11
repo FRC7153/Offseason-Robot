4a198412bbd868c93bb695760aaa073cd3dc9d05
mualexx <alexmusiitwa@gmail.com>
2024-04-11 11:51:37 -0400

    Please Work

---
+++ main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class Constants {
+    public static final class mecanumDriveConstants {
+        
+    }
+
+    public static final class HardwareConstants {
+        
+    }
+}
